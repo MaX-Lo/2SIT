99f13c806a25256e127297531340007951e2e2f9
Moritz Richter <moritz.richter@ymail.com>
2020-05-18 14:49:00 +0200

    complicated merge 1

--- src/main/java/de/twoSIT/models/Area.kt
+++ src/main/java/de/twoSIT/models/Area.kt
@@ -1,6 +1,6 @@
 package de.twoSIT.models
 
-class Area(val minLongitude: Double, val minLatitude: Double, val maxLongitude: Double, val maxLatitude: Double) {
+data class Area(val minLongitude: Double, val minLatitude: Double, val maxLongitude: Double, val maxLatitude: Double) {
     init {
         assert(minLongitude < maxLongitude)
         assert(minLatitude < maxLatitude)
