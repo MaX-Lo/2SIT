a390f1d2c7d255e573444b78c79404f5910b2fe9
MaX <lorenz_max@protonmail.com>
2020-05-25 14:17:26 +0200

    rename RawArea file

--- src/main/java/de/twoSIT/models/XmlWrapper.kt
+++ src/main/java/de/twoSIT/models/XmlWrapper.kt
@@ -5,11 +5,9 @@ import com.fasterxml.jackson.dataformat.xml.XmlMapper
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement
-import java.io.FileInputStream
 import java.io.StringReader
 import javax.xml.stream.XMLInputFactory
 import javax.xml.stream.XMLStreamReader
-import javax.xml.transform.Source
 
 
 class RawMember {
